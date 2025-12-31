f2d4dce6cfec997c299cec97efd962abaf774958
Wr3tchedTorch <ericmoura511@gmail.com>
2025-12-31 10:31:09 -0300

    messing around with vertex arrays

--- Terraria/main.cpp
+++ Terraria/main.cpp
@@ -26,13 +26,25 @@ int main()
 			if (keyPressed.scancode == sf::Keyboard::Scancode::Space)
 			{
 				cat.jump();
-				std::cout << "Jump!\n";
 			}
 		};
 
 	sf::Texture dirtTexture("Images/dirt.png");
-	sf::Sprite dirt(dirtTexture, {{18, 0}, {16, 16}});	
-	dirt.scale({ 3.0f, 3.0f });
+	
+	// create a triangle strip
+	sf::VertexArray triangleStrip(sf::PrimitiveType::TriangleStrip, 4);
+
+	// define it as a rectangle, located at (10, 10) and with size 100x100
+	triangleStrip[0].position = sf::Vector2f(10.f, 10.f);
+	triangleStrip[1].position = sf::Vector2f(10.f, 110.f);
+	triangleStrip[2].position = sf::Vector2f(110.f, 10.f);
+	triangleStrip[3].position = sf::Vector2f(110.f, 110.f);
+
+	// define its texture area to be a 25x50 rectangle starting at (0, 0)
+	triangleStrip[0].texCoords = sf::Vector2f(0.f, 0.f);
+	triangleStrip[1].texCoords = sf::Vector2f(0.f, 50.f);
+	triangleStrip[2].texCoords = sf::Vector2f(25.f, 0.f);
+	triangleStrip[3].texCoords = sf::Vector2f(25.f, 50.f);
 
 	int32_t currentTime = 0;
 	int32_t maxTime = 400;
@@ -60,13 +72,9 @@ int main()
 		cat.playAnimation(delta);
 
 		window.draw(cat);
-	
-		for (const auto& pos : dirtPositions)
-		{
-			dirt.setPosition(pos);
-			window.draw(dirt);
-		}
+		window.draw(triangleStrip, &dirtTexture);	
 
 		window.display();
 	}
 }
+
